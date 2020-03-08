09e9e0ca800ef7bc5ba8d7a02a1e2097a36e3a84
jesseKeeper <jesse.b.doelman@gmail.com>
2020-03-08 15:12:39 +0100

    Database aangepast

--- Database/testDB.sql
+++ Database/testDB.sql
@@ -5,8 +5,8 @@ USE `Bank3C`;
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS `KaartGegevens`;
-CREATE TABLE `KaartGegevens` (
+DROP TABLE IF EXISTS `Rekening`;
+CREATE TABLE `Rekening` (
     `iBanNR` varchar(15) NOT NULL,
     `CardUID` varchar(10) DEFAULT NULL,
     `Eigenaar_id` int(5) NOT NULL,
@@ -14,12 +14,10 @@ CREATE TABLE `KaartGegevens` (
     `Attemps` int(2) DEFAULT NULL,
     `Saldo` FLOAT(255, 2) DEFAULT null,
     primary key (`iBanNR`)
-    #CONSTRAINT `Eigenaar_id` FOREIGN KEY (`EigenaarInfo`),
-    #REFERENCES `EigenaarInfo` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 )CHARSET=utf8;
 
 BEGIN;
-INSERT INTO `KaartGegevens` VALUES ("TEST 12 3456789", "1234567890", 1, 9191, 3, 500.34), ("TEST 98 7654321", "9876543210", 2, 4567, 1, 99.21);
+INSERT INTO `Rekening` VALUES ("TEST 12 3456789", "1234567890", 1, 9191, 3, 500.34), ("TEST 98 7654321", "9876543210", 2, 4567, 1, 99.21), ("RABO TEST 123456", "963852741", 3, 4213, 3, 9842.69);
 COMMIT;
 
 DROP TABLE IF EXISTS `EigenaarInfo`;
@@ -33,13 +31,13 @@ CREATE TABLE `EigenaarInfo` (
 )CHARSET=utf8;
 
 BEGIN;
-INSERT INTO `EigenaarInfo` VALUES (1, "Jesse", "Doelman", "1234 NL", "17"), (2, "TEST", "TESTER", "4321 NL", "15");
+INSERT INTO `EigenaarInfo` VALUES (1, "Jesse", "Doelman", "1234 NL", "17"), (2, "TEST", "TESTER", "4321 NL", "15"), (3, "tester", "test", NULL, NULL);
 
 #test query met alles als resultaat
 SELECT concat(eigenaarinfo.Achternaam, ", ", eigenaarinfo.Voornaam) AS Rekeninghouder, 
 CONCAT(eigenaarinfo.postcode, ", ", eigenaarinfo.huisnummer) AS adres, 
-kaartgegevens.iBanNR as iBan, kaartgegevens.PIN, kaartgegevens.CardUID AS UID, 
-kaartgegevens.Attemps AS pogingen, kaartgegevens.Saldo 
+Rekening.iBanNR as iBan, Rekening.PIN, Rekening.CardUID AS UID, 
+Rekening.Attemps AS pogingen, Rekening.Saldo 
 
-FROM eigenaarinfo, kaartgegevens 
-WHERE eigenaarinfo.id = kaartgegevens.Eigenaar_id;
+FROM eigenaarinfo, Rekening 
+WHERE eigenaarinfo.id = Rekening.Eigenaar_id;
