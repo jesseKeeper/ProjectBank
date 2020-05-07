4918092b21ea6c530b3fa55a17a028bff611f2a0
jesseKeeper <jesse.b.doelman@gmail.com>
2020-05-07 17:28:25 +0200

    Database privileges toegevoegd in de .sql. Dit is de db met test Data wat geleegd moet worden voor de echte DB

--- Database/testDB.sql
+++ Database/testDB.sql
@@ -17,11 +17,9 @@ CREATE TABLE `rekening` (
     primary key (`iBan`)
 )CHARSET=utf8;
 
-
 INSERT INTO `rekening` VALUES ("TEST 12 3456789", "D2 42 3A 1B", 1, 9191, "1746992", 3, 500.34), ("TEST 98 7654321", "10987654321", 2, 4567, "1601794", 1, 99.21), 
 ("RABO TEST 12345", "96 5R 4C CE", 3, 4213, "1598752", 3, 9420.69), ("1234567890", "21 9C 87 6A", 1, 1111, "1508416", 3, 1.01), 
 ("Derde rekening", "Geen UID", 1, 8516, "1720802", 0, 1000.01), ("Tweede rekening", "Geen UID", 2, 6742, "1663231", 2, 999.42);
-
 
 DROP TABLE IF EXISTS `rekeninghouder`;
 CREATE TABLE `rekeninghouder` (
@@ -38,13 +36,9 @@ INSERT INTO `rekeninghouder` VALUES (1, "Jesse", "Doelman", "2000-10-29", "1234 
 (2, "TEST", "TESTER", "1999-1-1", "4321 NL", "15"), (3, "tester", "test", "1999-2-2",NULL, NULL), 
 (NULL, "testAuto", "incrementTest", "2020-3-11", NULL, NULL);
 
-#test query met alles als resultaat
-SELECT concat(rekeninghouder.achternaam, ", ", rekeninghouder.voornaam) AS Rekeninghouder, 
-rekeninghouder.geboortedatum,
-CONCAT(rekeninghouder.postcode, ", ", rekeninghouder.huisnummer) AS adres, 
-rekening.iBan, rekening.hashedPIN, rekening.cardUID AS UID, 
-rekening.attemps AS pogingen, rekening.saldo 
-
-FROM rekeninghouder, rekening 
-WHERE rekeninghouder.id = rekening.rekeninghouder_id
-ORDER BY rekening.rekeninghouder_id, rekening.saldo desc;
+# The needed user for the java Application
+CREATE USER `JavaApp`@`localhost` IDENTIFIED BY 'TheBestAnAppCanGet';
+GRANT SELECT (iBan, cardUID, PIN, hashedPIN, Salt, attemps, saldo), 
+UPDATE (attemps, saldo) ON bank3C.rekening 
+TO `JavaApp`@`localhost`;
+FLUSH PRIVILEGES;
