ab34133405d7b964afb2785f566e709ba0e1f3d0
timkaradeniz <60885708+timkaradeniz@users.noreply.github.com>
2020-05-28 09:41:39 +0200

    NIEUWEGUI

---
+++ NIEUWEGUI/build/classes/NewJFrame.rs
@@ -0,0 +1,11 @@
+guitest.NewJFrame$2
+guitest.NewJFrame$3
+guitest.NewJFrame$4
+guitest.NewJFrame$5
+guitest.NewJFrame$6
+guitest.NewJFrame$10
+guitest.NewJFrame$7
+guitest.NewJFrame$8
+guitest.NewJFrame$9
+guitest.NewJFrame
+guitest.NewJFrame$1
